d1eb5240f5b98a6fb10a9dfe4b0e0cf1f5d15986
Helamã Y. Barbour <helamashi@gmail.com>
2026-01-09 16:25:52 -0300

    Finished the W01 Code: Dynamic Arrays activity.

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,25 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // So the code will get a number and the lenght as inputs, so if the user provides de number 3 and an lenght of 3
+        // the output should be an array with the values 3, 6, 9.
+        // To do this I will:
+        // 1. I need to create an array of doubles with the length provided by the user.
+        // 2. I will create a for loop that will iterate from 0 to length provided by the user
+        // So it would be for int i = 0; while i is less then the length; i++
+        // 3. Inside the for loop, the code will calculate the multiple of the number by multiplying it by (i + 1)
+        // So it would be number * (i + 1) - The Plus one is necessary because i will begging as 0 not 1.
+        // 4. Then the result will be stored in the array at the index of i since it's following the order 0, 1, 2, etc.
+        // 5. After the loop is done, the code will return the array
+
+        var multiplesArray = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            multiplesArray[i] = number * (i + 1);
+        }
+
+        return multiplesArray; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +47,26 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Basically the code needs to move to the right the elements of a list the amount of times specified by the user.
+        // To do this I will:
+        // 1. Create a for loop
+        // The for loop will be as follows: for int i = 0; while i is less than amount; i++
+        // 2. Inside the for loop, the code will need to increment the indexes of the list to the right.
+        // The only exception will be the last element of the list, which will need to have the index of 0.
+        // For that I can remove the last element of the list and then add it to the start of the list.
+        // 3. To remove the last element I will need to store it in a varible called lastElement.
+        // 4. Then I will remove the last element from the list using data.RemoveAt(data.Count - 1);
+        // The .Count counts the amount of elements in a list, so if we do the count and subtract 1 we will get the index of the last element.
+        // 5. I will add the lastElement to the start of the list using data.Insert(0, lastElement);
+        // 6. After the for loop is done, the list will be rotated.
+
+        for (int i = 0; i < amount; i++)
+        {
+            int lastElement = data[data.Count - 1];
+            data.RemoveAt(data.Count - 1);
+            data.Insert(0, lastElement);
+            // In C# when an list have "space" for a new element, no matter where you inset the element, the other elements from the right will move and do the indexes automatically change.
+        }
     }
 }
